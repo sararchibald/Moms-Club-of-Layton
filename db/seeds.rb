085ff279da9bd0e26eed3e785ae558cadc7eea32
Michael Archibald <micharch54@gmail.com>
2015-06-27 17:33:02 -0600

    fixing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Spina::User.create(name:'Sara',email:'sararchibald@gmail.com',password:'Superman1',password_confirmation:'Superman1',admin:true)
-Spina::Account.create(name: 'Sara')
+Spina::User.destroy_all
+Spina::User.create name: "Bram", email: "sararchibald@gmail.com", password: "Superman1", password_confirmation: "Superman1", admin: true
+
+Spina::Account.destroy_all
+Spina::Account.create name: "Website"
+
+Spina::Page.destroy_all
